af3cc876a36fb9428b268b1f8493cb73b51bdfec
Jon Polit <jpolit@agero.com>
2019-06-07 11:52:35 -0400

    lookup mapping from numbers to letters

---
+++ wordification/helpers.py
@@ -0,0 +1,9 @@
+# derived from a google image search of an "old fashioned" phone 
+letters_from_numbers_lookup = {'2': ['A', 'B', 'C'],
+                               '3': ['D', 'E', 'F'],
+                               '4': ['G', 'H', 'I'],
+                               '5': ['J', 'K', 'L'],
+                               '6': ['M', 'N', 'O'],
+                               '7': ['P', 'Q', 'R', 'S'],
+                               '8': ['T', 'U', 'V'],
+                               '9': ['W', 'X', 'Y' 'Z']}
---
+++ wordification/wordification.py
@@ -0,0 +1,16 @@
+import wordification.helpers as helpers
+
+
+def number_to_words(phone_num: str) -> str:
+    raise NotImplementedError
+
+
+def words_to_numbers(phone_words: str) -> str:
+    raise NotImplementedError
+
+
+def all_wordifications(phone_num: str) -> str:
+    raise NotImplementedError
+
+
+
