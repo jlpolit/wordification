c68c752122d192639637459888b9ca0002b3db81
Jon Polit <jpolit@agero.com>
2019-06-07 12:39:54 -0400

    add helper to format output from list

--- wordification/helpers.py
+++ wordification/helpers.py
@@ -10,7 +10,17 @@ letters_from_numbers_lookup = {'2': ['A', 'B', 'C'],
                                '9': ['W', 'X', 'Y' 'Z']}
 
 
-def is_valid_word(word_to_check, min_length=1):
+numbers_from_letters_lookup = {'A': '2', 'B': '2', 'C': '2',
+                               'D': '3', 'E': '3',  'F': '3',
+                               'G': '4', 'H': '4',  'I': '4',
+                               'J': '5', 'K': '5',  'L': '5',
+                               'M': '6', 'N': '6', 'O': '6',
+                               'P': '7', 'Q': '7', 'R': '7', 'S': '7',
+                               'T': '8', 'U': '8', 'V': '8',
+                               'W': '9', 'X': '9', 'Y': '9', 'Z': '9'}
+
+
+def is_valid_word(word_to_check: str, min_length=1) -> bool:
     if type(word_to_check) is not str:
         raise ValueError("Non-string entered")
     if len(word_to_check) < min_length:
@@ -19,3 +29,29 @@ def is_valid_word(word_to_check, min_length=1):
         english_word_lookup = enchant.Dict("en_US")
         return english_word_lookup.check(word_to_check)
 
+
+def format_phone_number(phone_digit_list: list) -> str:
+    #TODO: we should actually probably check that each 'digit' is a string rather than forcing it
+    out_str = ''
+    # length check
+    if (len(phone_digit_list) not in [10, 11]) or (type(phone_digit_list) is not list):
+        raise ValueError("not a valid phone number")
+    # country code
+    if len(phone_digit_list) == 11:
+        out_str = (phone_digit_list.pop(0) + '-')
+
+    # zipcode
+    for digit in phone_digit_list[:3]:
+        out_str += str(digit)
+    out_str += '-'
+
+    # the...next three digits (I'm sure this has a name)
+    for digit in phone_digit_list[3:6]:
+        out_str += str(digit)
+    out_str += '-'
+
+    # and the last four
+    for digit in phone_digit_list[6:]:
+        out_str += str(digit)
+
+    return out_str
--- wordification/wordification.py
+++ wordification/wordification.py
@@ -6,7 +6,21 @@ def number_to_words(phone_num: str) -> str:
 
 
 def words_to_numbers(phone_words: str) -> str:
-    raise NotImplementedError
+    """
+
+    :param phone_words: a string representation of a phone number that mixes words and digits
+    :return: the number corresponding to this phone #
+    """
+    # drop hyphens, put everything into a list
+    character_list = [x for x in phone_words.replace('-', '')]
+    out_list = []
+    for c in character_list:
+        if c.isdigit():
+            out_list.append(c)
+        else:
+            out_list.append(helpers.numbers_from_letters_lookup[c.upper()])
+
+    return helpers.format_phone_number(out_list)
 
 
 def all_wordifications(phone_num: str) -> str:
