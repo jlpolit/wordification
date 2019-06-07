3a14c30e5154eca903febcf59083027251ff9585
Jon Polit <jpolit@agero.com>
2019-06-07 12:09:42 -0400

    start tests, add enligh word lookup

---
+++ tests/test_wordification.py
@@ -0,0 +1,17 @@
+import unittest
+from wordification.wordification import number_to_words, words_to_numbers, all_wordifications
+
+
+class TestWordificationFunctions(unittest.TestCase):
+    def test_number_to_words(self):
+        return True
+
+    def test_words_to_numbers(self):
+        return True
+
+    def test_all_wordifications(self):
+        return True
+
+
+if __name__ == '__main__':
+    unittest.main()
--- wordification/helpers.py
+++ wordification/helpers.py
@@ -1,4 +1,5 @@
-# derived from a google image search of an "old fashioned" phone 
+import enchant
+# derived from a google image search of an "old fashioned" phone
 letters_from_numbers_lookup = {'2': ['A', 'B', 'C'],
                                '3': ['D', 'E', 'F'],
                                '4': ['G', 'H', 'I'],
@@ -7,3 +8,14 @@ letters_from_numbers_lookup = {'2': ['A', 'B', 'C'],
                                '7': ['P', 'Q', 'R', 'S'],
                                '8': ['T', 'U', 'V'],
                                '9': ['W', 'X', 'Y' 'Z']}
+
+
+def is_valid_word(word_to_check, min_length=1):
+    if type(word_to_check) is not str:
+        raise ValueError("Non-string entered")
+    if len(word_to_check) < min_length:
+        return False
+    else:
+        english_word_lookup = enchant.Dict("en_US")
+        return english_word_lookup.check(word_to_check)
+
